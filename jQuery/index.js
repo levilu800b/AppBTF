6cb13eb22b63e1c819dc9797036c9b1717cf98c1
levilu800b <levilu800b@gmail.com>
2022-05-21 11:35:02 +0100

    first

---
+++ jQuery/index.js
@@ -0,0 +1,70 @@
+// $('h1').css('color', 'red'); // it can also be written as jquery('h1').css('color', 'red');
+// $('h1').text('Hello World');
+// $('button').html('<em>Hey</em>');
+
+// $('a').attr('href', 'https://www.bing.com'); // how to change the href attribute of the anchor tag
+
+// $('h1').click(function () {
+// 	$('h1').css('color', 'purple');
+// }); // how to add an event listener to an element
+
+// $('button').click(function () {
+// 	$('h1').css('color', 'purple');
+// }); // multiple event listeners
+
+// $('input').keypress(function (event) {
+// 	$('h1').text(event.key);
+// }); // makes the h1 text the key that was pressed
+
+// $('h1').on('mouseenter', function () {
+// 	$('h1').css('color', 'purple');
+// }); // how to use mouse to change the color of the h1
+
+// $('h1').before('<button>Click Me</button>'); // how to add a button before the h1
+// $('h1').after('<button>Click Me</button>'); // how to add a button after the h1
+// $('h1').prepend('<button>Click Me</button>'); // how to add a button before the h1
+// $('h1').append('<button>Click Me</button>'); // how to add a button after the h1
+
+// $('button').remove(); // how to remove a button
+
+// $('button').on('click', function () {
+// 	$('h1').hide();
+// }); // how to hide an element
+
+// $('h1').show(); // how to show an element
+
+// $('button').on('click', function () {
+// 	$('h1').toggle();
+// }); // how to toggle an element
+
+// $('button').on('click', function () {
+// 	$('h1').fadeOut();
+// }); // how to fade out an element
+
+// $('button').on('click', function () {
+// 	$('h1').fadeIn();
+// }); // how to fade in an element
+
+// $('button').on('click', function () {
+// 	$('h1').fadeToggle();
+// }); // how to fade toggle an element
+
+// $('button').on('click', function () {
+// 	$('h1').slideUp();
+// }); // how to slide up an element
+
+// $('button').on('click', function () {
+// 	$('h1').slideDown();
+// }); // how to slide down an element
+
+// $('button').on('click', function () {
+// 	$('h1').slideToggle();
+// });
+
+// $('button').on('click', function () {
+// 	$('h1').animate({ opacity: 0.5 });
+// }); // how to animate an element
+
+// $('button').on('click', function () {
+// 	$('h1').slideUp().slideDown().animate({ opacity: 0.5 });
+// }); // how to chain animations
